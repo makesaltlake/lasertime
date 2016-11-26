62d945d8dda2afa618fdab3754090d03ad507191
Alex Boyd <alex@opengroove.org>
2016-11-26 01:55:27 -0700

    run webpack build on deploy
    the nodejs buildpack doesn't install dev dependencies, so we have
    to list webpack et al. as ordinary ones, which kinda sucks

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'index.jsx'),
+  entry: path.resolve(__dirname, 'webpack/index.jsx'),
   output: {
-    path: path.resolve(__dirname, '../public/generated'),
+    path: path.resolve(__dirname, 'public/generated'),
     filename: 'bundle.js'
   },
   module: {
