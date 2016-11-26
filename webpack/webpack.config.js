981cb82f8cd48dddc25e1f7442a5e3ba2037b107
Alex Boyd <alex@opengroove.org>
2016-11-26 01:16:09 -0700

    initial webpack configuration

---
+++ webpack/webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, '../public/generated'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
